423620b1c37b0e4192209e5a6a8d1579b43b375c
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2024-07-22 01:42:23 -0500

    added Rival2 to Route 117

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -673,7 +673,7 @@
 #define FLAG_HIDE_OUTDOOR_DEWFORD_RIVALS        0x276
 #define FLAG_HIDE_SEASHORE_HOUSE_RIVAL2         0x277
 #define FLAG_HIDE_BIKE_SHOP_RIVALS              0x278
-#define FLAG_UNUSED_0x279  0x279 // Unused Flag
+#define FLAG_HIDE_ROUTE_117_RIVAL2              0x279
 #define FLAG_UNUSED_0x27A  0x27A // Unused Flag
 #define FLAG_UNUSED_0x27B  0x27B // Unused Flag
 #define FLAG_UNUSED_0x27C  0x27C // Unused Flag
