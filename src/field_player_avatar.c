db04b5eb81bb37a5b978876b6e2fcec16e966a25
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2025-01-18 18:24:09 -0600

    modified the bg colors
    and restored ow fishing dots

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1775,7 +1775,7 @@ static bool8 Fishing_InitDots(struct Task *task)
     u32 randVal;
 
     LoadMessageBoxAndFrameGfx(0, TRUE);
-    task->tStep = 5;
+    task->tStep++;
     task->tFrameCounter = 0;
     task->tNumDots = 0;
     randVal = Random();
@@ -1948,50 +1948,13 @@ static bool8 Fishing_SetUpPokemon(struct Task *task)
     if (task->tFrameCounter != 0)
     {
         FishingWildEncounter(task->tFishingRod);
-        //FadeScreen(FADE_TO_BLACK, 0);
         BeginNormalPaletteFade(PALETTES_ALL, 0, 0, 16, RGB_BLACK);
         task->tStep++;
         task->tFrameCounter = 0;
     }
     return FALSE;
 }
-/*
-static bool8 Fishing_StartEncounter(struct Task *task)
-{
-    if (task->tFrameCounter == 0)
-        AlignFishingAnimationFrames();
 
-    RunTextPrinters();
-
-    if (task->tFrameCounter == 0)
-    {
-        if (!IsTextPrinterActive(0))
-        {
-            struct ObjectEvent *playerObjEvent = &gObjectEvents[gPlayerAvatar.objectEventId];
-
-            ObjectEventSetGraphicsId(playerObjEvent, task->tPlayerGfxId);
-            ObjectEventTurn(playerObjEvent, playerObjEvent->movementDirection);
-            if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
-                SetSurfBlob_PlayerOffset(gObjectEvents[gPlayerAvatar.objectEventId].fieldEffectSpriteId, FALSE, 0);
-            gSprites[gPlayerAvatar.spriteId].x2 = 0;
-            gSprites[gPlayerAvatar.spriteId].y2 = 0;
-            ClearDialogWindowAndFrame(0, TRUE);
-            task->tFrameCounter++;
-            return FALSE;
-        }
-    }
-
-    if (task->tFrameCounter != 0)
-    {
-        gPlayerAvatar.preventStep = FALSE;
-        UnlockPlayerFieldControls();
-        FishingWildEncounter(task->tFishingRod);
-        RecordFishingAttemptForTV(TRUE);
-        DestroyTask(FindTaskIdByFunc(Task_Fishing));
-    }
-    return FALSE;
-}
-*/
 static bool8 Fishing_StartMinigame(struct Task *task)
 {
     if (!gPaletteFade.active)
